10c3d57783eac7be6b95a888a2e3a80de5b6069b
perchik_air <1919ars@gmail.com>
2023-04-11 17:08:12 +0300

    more

--- algs/m_dssa.py
+++ algs/m_dssa.py
@@ -5,24 +5,14 @@ from algs.alg_objects import *
 from functions import *
 
 
-class CaDsaMstAlgAgent(AlgAgent):
+class DssaAlgAgent(AlgAgent):
     def __init__(self, sim_agent):
-        super(CaDsaMstAlgAgent, self).__init__(sim_agent)
-        self.next_possible_pos, self.next_possible_action = None, None
-        self.next_action = -1
+        super(DssaAlgAgent, self).__init__(sim_agent)
         self.beliefs = {}
-
-        # states
-        """
-        first
-        v
-        f_move -> exchange -> f_exchange -> plan -> f_plan -> ca_correction -> f_ca_correction -> move
-        ^                                                                                            v
-        <-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-
-        """
+        self.state = 'first'
         self.sync_time = 0
         self.state_counter = 0
-        self.state = 'first'
+        self.next_possible_pos, self.next_possible_action = None, None
 
     def reset_beliefs(self):
         self.beliefs = {}
@@ -42,6 +32,31 @@ class CaDsaMstAlgAgent(AlgAgent):
                 if self.state == content['state']:
                     self.beliefs[from_a_name]['state'] = content['state']
                 self.beliefs[from_a_name]['next_possible_pos'] = content['next_possible_pos']
+
+    def all_states_aligned(self):
+        for agent_name, belief in self.beliefs.items():
+            state = belief['state']
+            if state != self.state:
+                return False
+        return True
+
+    def get_send_order(self, show_next_pos=True):
+        """
+        SEND ORDER: message -> [(from, to, s_time, content), ...]
+        """
+        if not show_next_pos:
+            self.next_pos = None
+            self.next_possible_action = None
+            self.next_possible_pos = None
+        messages = []
+        for agent in self.all_agents:
+            content = {
+                'state': self.state,
+                'next_possible_pos': self.next_possible_pos,
+            }
+            new_message = (self.name, agent['name'], self.step_count, content)
+            messages.append(new_message)
+        return messages
 
     def decide_next_possible_move(self):
         self.next_possible_pos, self.next_possible_action = None, None
@@ -64,67 +79,37 @@ class CaDsaMstAlgAgent(AlgAgent):
         if self.next_possible_pos is None or self.next_possible_action is None:
             raise RuntimeError()
 
-    def action_without_collisions(self):
-        # if there is a possible collision -> stay on place
-        for nei in self.nei_agents:
-            nei_name = nei['name']
-            nei_next_possible_pos = self.beliefs[nei_name]['next_possible_pos']
-            if nei_next_possible_pos is None:
-                raise RuntimeError()
-            if self.next_possible_pos.xy_name == nei_next_possible_pos.xy_name:
-                return 0
-            if self.next_possible_pos.xy_name == nei['pos'].xy_name:
-                return 0
-        return self.next_possible_action
-
-    def get_send_order(self, show_next_pos=True):
-        """
-        SEND ORDER: message -> [(from, to, s_time, content), ...]
-        """
-        if not show_next_pos:
-            self.next_pos = None
-            self.next_action = None
-            self.next_possible_action = None
-            self.next_possible_pos = None
-        messages = []
-        for agent in self.all_agents:
-            content = {
-                'state': self.state,
-                'next_possible_pos': self.next_possible_pos,
-            }
-            new_message = (self.name, agent['name'], self.step_count, content)
-            messages.append(new_message)
-        return messages
-
-    def all_states_aligned(self):
-        for agent_name, belief in self.beliefs.items():
-            state = belief['state']
-            if state != self.state:
-                return False
-        return True
-
-    def received_all_next_possible_pos(self):
-        for nei in self.nei_agents:
-            nei_name = nei['name']
-            nei_next_possible_pos = self.beliefs[nei_name]['next_possible_pos']
-            if nei_next_possible_pos is None:
-                return False
-        return True
-
     def state_first(self):
         self.reset_beliefs()
-        self.state = 'f_move'
+        self.state = 'plan'
         move_order = -1
         send_order = self.get_send_order(show_next_pos=False)
         return move_order, send_order
 
-    def state_move(self):
-        if self.state != 'move':
-            raise RuntimeError()
+    def state_plan(self):
+        # TODO
+        """
+        DSSA:
+        Exchange next possible positions until the solution without collisions will be found.
+        Each time exclude the problematic position.
+        """
+        self.decide_next_possible_move()
+        self.state = 'f_plan'
+        move_order = -1
+        send_order = self.get_send_order()
+        return move_order, send_order
 
+    def state_f_plan(self):
+        move_order = -1
+        send_order = self.get_send_order()
+        if self.all_states_aligned():
+            self.state = 'move'
+            return self.next_possible_action, send_order
+        return move_order, send_order
+
+    def state_move(self):
         if self.is_moving:
             move_order, send_order = -1, []
-            self.sync_time = self.step_count
             return move_order, send_order  # wait
         self.state_counter += 1
         self.state = 'f_move'
@@ -137,37 +122,8 @@ class CaDsaMstAlgAgent(AlgAgent):
         send_order = self.get_send_order(show_next_pos=False)
         if self.all_states_aligned():
             self.reset_beliefs()
+            self.sync_time = self.step_count
             self.state = 'plan'
-        return move_order, send_order
-
-    def state_plan(self):
-        self.decide_next_possible_move()
-        self.state = 'f_plan'
-        move_order = -1
-        send_order = self.get_send_order()
-        return move_order, send_order
-
-    def state_f_plan(self):
-        move_order = -1
-        send_order = self.get_send_order()
-        if self.all_states_aligned():
-            self.state = 'ca_correction'
-        return move_order, send_order
-
-    def state_ca_correction(self):
-        move_order = -1
-        send_order = self.get_send_order()
-        if self.received_all_next_possible_pos():
-            self.next_action = self.action_without_collisions()
-            self.state = 'f_ca_correction'
-        return move_order, send_order
-
-    def state_f_ca_correction(self):
-        move_order = -1
-        send_order = self.get_send_order()
-        if self.all_states_aligned():
-            self.state = 'move'
-            return self.next_action, send_order
         return move_order, send_order
 
     def process(self, observation):
@@ -177,39 +133,36 @@ class CaDsaMstAlgAgent(AlgAgent):
         if self.state == 'first':
             move_order, send_order = self.state_first()
 
-        elif self.state == 'move':
-            move_order, send_order = self.state_move()
-
-        elif self.state == 'f_move':
-            move_order, send_order = self.state_f_move()
-
         elif self.state == 'plan':
             move_order, send_order = self.state_plan()
 
         elif self.state == 'f_plan':
             move_order, send_order = self.state_f_plan()
 
-        elif self.state == 'ca_correction':
-            move_order, send_order = self.state_ca_correction()
+        elif self.state == 'move':
+            move_order, send_order = self.state_move()
 
-        elif self.state == 'f_ca_correction':
-            move_order, send_order = self.state_f_ca_correction()
+        elif self.state == 'f_move':
+            move_order, send_order = self.state_f_move()
 
         else:
             raise RuntimeError('unknown state')
 
+        # move_order = self.get_move_order()
+        # send_order = self.get_send_order()
+        # move_order, send_order = -1, []
         return move_order, send_order
 
 
-class CaDsaMstAlg:
+class DssaAlg:
     def __init__(self):
-        self.name = 'CADSA'
+        self.name = 'DSSA'
         self.agents, self.agents_dict = None, None
 
     def create_entities(self, sim_agents, sim_targets):
         self.agents, self.agents_dict = [], {}
         for sim_agent in sim_agents:
-            new_agent = CaDsaMstAlgAgent(sim_agent)
+            new_agent = DssaAlgAgent(sim_agent)
             self.agents.append(new_agent)
             self.agents_dict[new_agent.name] = new_agent
 
@@ -219,11 +172,12 @@ class CaDsaMstAlg:
     def get_actions(self, observations):
         actions = {}
         # state_counter
-        # print(f' ------------------------------ step: {observations["step_count"]} ------------------------------ ')
+        print(f' ------------------------------ step: {observations["step_count"]} ------------------------------ ')
         for agent in self.agents:
             move_order, send_order = agent.process(observations[agent.name])
             actions[agent.name] = {'move': move_order, 'send': send_order}
             # print(f"{agent.name}'s state counter: {agent.state_counter}, state: {agent.state}")
+
         return actions
 
     def get_info(self):
@@ -234,7 +188,7 @@ class CaDsaMstAlg:
 def main():
     # set_seed(random_seed_bool=False, i_seed=353)
     set_seed(random_seed_bool=True)
-    alg = CaDsaMstAlg()
+    alg = DssaAlg()
     # test_mst_alg(alg, to_render=False)
     # test_mst_alg(alg, to_render=True, plot_every=10)
     # set_seed(True, 353)
@@ -243,9 +197,9 @@ def main():
         n_agents=30,
         n_targets=10,
         to_render=True,
-        plot_every=50,
-        n_problems=5,
-        max_steps=1200,
+        plot_every=10,
+        n_problems=3,
+        max_steps=120,
     )
 
 
